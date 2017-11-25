6015e923b6313ba2f002ba757ea8620598820255
jwojciec <mail@jwojciechowski.pl>
2017-11-25 23:00:42 +0100

    example unit tests added

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Project generated with SPRING INITIALIZR https://start.spring.io/
 
 Finish this project following these steps:
 1. Implement missing methods in SimpleListRepository & UserController
-2. Add unit tests in UserControllerTest & SimpleListRepositoryTest
-3. Use H2 database as your repository. Check tutorial on https://spring.io/guides/gs/accessing-data-jpa/
+2. Add unit tests in UserControllerTest & SimpleListRepositoryTest. Check
+[Spring Testing](https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-testing.html)
+3. Use H2 database as your repository. Check tutorial on
+[Spring Accessing data](https://spring.io/guides/gs/accessing-data-jpa/)
 4. Add Angular2 front-end. This is going to be our REST client.
 5. Upload project to cloud https://www.heroku.com/
 
--- src/test/java/com/example/training/controllers/UserControllerTest.java
+++ src/test/java/com/example/training/controllers/UserControllerTest.java
@@ -1,7 +1,63 @@
 package com.example.training.controllers;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.training.model.User;
+import com.example.training.repository.SimpleListRepository;
+import com.google.common.collect.ImmutableList;
+
+@RunWith(SpringJUnit4ClassRunner.class)
 public class UserControllerTest {
+    private static final String ALL_USERS_PATH = "src/test/resources/user/allUsers.json";
+    private static final String SINGLE_USER_PATH = "src/test/resources/user/singleUser.json";
+    private static final User TEST_USER1 = new User("2a3993ee-d74e-42ac-af90-9390642db803", "John", "Doe");
+    private static final User TEST_USER2 = new User("8644d523-eaf2-412b-87b9-82ceba72ec6b", "Foo", "Bar");
+    private MockMvc mockMvc;
+
+    @Mock
+    private SimpleListRepository simpleListRepository;
+
+    @Before
+    public void setup() {
+        this.mockMvc = standaloneSetup(new UserController(simpleListRepository)).build();
+
+        when(simpleListRepository.getAllUsers()).thenReturn(ImmutableList.of(TEST_USER1, TEST_USER2));
+        when(simpleListRepository.getUser(1)).thenReturn(TEST_USER1);
+    }
+
+    @Test
+    public void When_GettingAllUsers_Expect_CorrectResponse() throws Exception {
+        this.mockMvc.perform(get("/users"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(readJsonFile(ALL_USERS_PATH)));
+    }
+
+    @Test
+    public void When_GettingUserById_Expect_CorrectResponse() throws Exception {
+        this.mockMvc.perform(get("/users/1"))
+            .andExpect(status().isOk())
+            .andExpect(content().json(readJsonFile(SINGLE_USER_PATH)));
+    }
+
     //ToDo implement unit tests for UserController
+
+    private static String readJsonFile(String path) throws IOException, ParseException {
+        return new JSONParser().parse(new FileReader(path)).toString();
+    }
 }
